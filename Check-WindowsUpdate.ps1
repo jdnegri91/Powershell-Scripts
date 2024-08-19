581a745be2faacff37c43fad2564907fc5c00b03
Julian Negri <42880736+jdnegri91@users.noreply.github.com>
2024-08-19 19:53:28 -0400

    Add files via upload
    Script to check PS Windows Update Module for Desired Windows Update and Install

---
+++ Check-WindowsUpdate.ps1
@@ -0,0 +1,36 @@
+﻿#Check for Windows updates
+Get-Date | Write-output
+Function CheckFeatureUpdates(){
+    If(-not(Get-InstalledModule PSWindowsUpdate -ErrorAction silentlycontinue)){
+    Install-packageprovider -name nuget -minimumversion 2.8.5.201 -force | Out-Null
+    Install-Module PsWindowsUpdate -Confirm:$False -Force | write-host "Installing Update Module to "$env:COMPUTERNAME" "
+    }
+    #Get All Results
+    $Results = Get-Windowsupdate -NotCategory Driver
+    #Create Keywords for Update Search
+    $update = @{Update1 = "*Feature*" ; Update2 = "*Update for Windows 10*"}
+     
+    foreach ($key in $update.values){
+        $results | foreach-object {
+            if ($_.title -like $key){
+                $_
+            }
+        }
+    }
+}
+
+#Check for Updates then Install Feature Update if present or Regular Update
+$Updates = CheckFeatureUpdates
+if($Updates){
+    if($Updates | where title -like *"Feature update"*)
+        {
+        $KBs = $Updates | where title -like *"Feature update"* | select KB
+        Install-WindowsUpdate -KBArticleID $KBs -AcceptAll -ForceDownload -ForceInstall -IgnoreReboot 
+        }
+    Else{
+        $KBs = $Updates | where title -like *"Update for Windows 10"* | select KB
+        Install-WindowsUpdate -KBArticleID $KBs -AcceptAll -ForceDownload -ForceInstall -IgnoreReboot 
+        }
+}
+
+Else {Write-Host "Desired Updates are not Available"}
