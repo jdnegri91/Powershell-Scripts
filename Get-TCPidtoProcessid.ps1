f899d13247a3583e4cbde1744590d26db76eff5a
Julian Negri <42880736+jdnegri91@users.noreply.github.com>
2024-08-19 20:04:57 -0400

    Add files via upload
    Script to match running processes ids with their open listening TCP ports

---
+++ Get-TCPidtoProcessid.ps1
@@ -0,0 +1,19 @@
+﻿#Get Running Processes
+$Processes = Get-Process | Select-Object ProcessName,ID
+#Get Listening Ports
+$TCP = Get-NetTCPConnection -State Listen -Verbose
+#Capture Processes Ids
+$TCPID = $TCP.OwningProcess
+#Loop through Processes IDS for matching running process
+For ($i=0; $i -le $tcp.Length; $i++){
+
+    $Processes | ForEach-Object { 
+
+    
+    if($_.Id -eq $tcpid[$i]) 
+    
+    {Write-Host $_.processname "Has Port" $TCP.localport[$i] "open" }  
+
+}
+}
+ 
