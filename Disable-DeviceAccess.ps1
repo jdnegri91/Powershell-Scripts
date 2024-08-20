585b5b0c82230b44152c18da030dbe6d2e76d55b
Julian Negri <42880736+jdnegri91@users.noreply.github.com>
2024-08-19 20:23:49 -0400

    Add files via upload
    Script to force BitLocker recovery and lock out user from signing in via PIN or Password

---
+++ Disable-DeviceAccess.ps1
@@ -0,0 +1,9 @@
+﻿#Remove Password Login from Device
+New-ItemProperty -path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Authentication\Credential Providers\{60B78E88-EAD8-445C-9CFD-0B87F74EA6CD}'-name 'Disabled' -value '1' -PropertyType 'DWord' -Force
+#Clear TPM of Windows Hello Credentials
+Clear-TPM
+#Force Bitlocker Recovery 
+manage-bde -forcerecovery C:
+manage-bde -protectors -enable C:
+#Restart
+shutdown -f -r -t 0
