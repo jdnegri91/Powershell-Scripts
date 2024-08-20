a7e4f8476657043890174f3292b91364383a5ce2
Julian Negri <42880736+jdnegri91@users.noreply.github.com>
2024-08-19 20:29:49 -0400

    Add files via upload
    Script that will deploy Azcopy to an endpoint, stage a directory, copy contents, and then upload.

---
+++ Deploy-AZCopyToAzureBlob.ps1
@@ -0,0 +1,30 @@
+﻿#Create Directory
+If (Test-Path -Path "C:\Windows\Secure"){
+    While(Get-ChildItem C:\windows\secure\ | Where-Object Name -like *"azcopy_windows_amd64"*){
+    $foldername = "C:\Windows\Secure\" + (Get-ChildItem C:\windows\secure\ | Where-Object Name -like *"azcopy_windows_amd64"*).Name
+    Remove-Item $filename -Recurse
+    }   
+}
+Else{
+New-Item -Type Directory -Path "C:\Windows\Secure" | Out-Null
+}
+
+#download Azcopy and unzip
+Invoke-WebRequest -uri "https://aka.ms/downloadazcopy-v10-windows" -OutFile "C:\Windows\Secure\azcopy.zip"
+Expand-Archive C:\Windows\Secure\azcopy.zip C:\Windows\Secure\ -force
+$foldername = "C:\Windows\Secure\" + (Get-ChildItem C:\windows\secure\ | Where-Object Name -like *"azcopy_windows_amd64"*).Name
+Set-Location $foldername
+ 
+
+
+#stage upload directory path
+$name = hostname
+$date = get-date -Format yyyyMMdd
+$dir = "C:\windows\Secure\$($name)-$($date)"
+mkdir $dir -Force
+
+#copy desired contents to path
+cp "<File Path>" $dir -Recurse -Force
+
+#upload to Azure Blob
+.\azcopy.exe copy $dir "<Blob Storage Path>" --recurse
