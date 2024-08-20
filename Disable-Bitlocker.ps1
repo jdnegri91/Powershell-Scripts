b026b87aff1a7e628220851441c88928e6591f3f
Julian Negri <42880736+jdnegri91@users.noreply.github.com>
2024-08-19 20:10:29 -0400

    Add files via upload
    Disables Bitlocker and Decrypts disk on Device

---
+++ Disable-Bitlocker.ps1
@@ -0,0 +1,19 @@
+﻿#Disable Bitlocker
+Manage-bde -off C:
+
+
+#Check Status until finished decrypting
+$Status = $false
+While($Status -eq $false){
+    $bitlocker = Manage-bde -status
+    $bitlockerstatus = $bitlocker| Where-Object {$_ -match 'Conversion Status'}
+    If($bitlockerstatus -like "*Decryption in Progress*"){
+        Write-output "Disk is Decrypting"
+        sleep 10
+        }
+    else{
+        Write-output "Disk Decrypted"
+        $Status = $True
+    }
+
+}
